bc7ed1b78d97497193f36faef15534da6e4be2cd
tinxte <150742495+Tinxte@users.noreply.github.com>
2025-05-17 16:14:33 -0500

    Rune boilerplate set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import path from "node:path"
+import rune from "rune-sdk/vite"
+import { defineConfig } from "vite"
+import { qrcode } from "vite-plugin-qrcode"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "", // Makes paths relative
+  plugins: [
+    qrcode(), // only applies in dev mode
+    react(),
+    rune({
+      logicPath: path.resolve("./src/logic.ts"),
+      minifyLogic: false, // This flag can be used if your logic reaches the allowed limit. However, it will make it significantly more difficult to detect validation issues
+      ignoredDependencies: [],
+    }),
+  ],
 })
